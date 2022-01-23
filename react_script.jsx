fd6132b51e392bce6c2cbf1cbcf1ffe0e36f3f4c
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-23 17:50:49 +0530

    updated our react app

--- react_script.jsx
+++ react_script.jsx
@@ -3,22 +3,23 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return (
+            // Jsx syntax will not work without transpiling so needed a transpiler like babel;
+            <button onClick={() => this.setState({ liked: true })}>
+                Like
+            </button>
+        );
+    }
 }
 
 const domContainer = document.querySelector('#reactRoot');
