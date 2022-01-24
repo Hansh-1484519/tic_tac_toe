5b11ec36cdd187564de463d4cb63545fe5dbf0c4
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-24 12:35:01 +0530

    added styling to game

--- react_script.jsx
+++ react_script.jsx
@@ -43,8 +43,14 @@ class Game extends React.Component{
 
     render(){
         return(
-            <div>
-                <Board/>
+            <div className = "game">
+                <div className = "game-board">
+                    <Board/>
+                </div>
+                <div className = "game-info">
+                    <div>{   }</div>
+                    <ol>  { }  </ol>
+                </div>
             </div>
         );
     }
