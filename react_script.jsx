c0398442533bad481ae161bcf1380edb6e3f9171
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-25 10:54:44 +0530

    add helper function to determine the winner

--- react_script.jsx
+++ react_script.jsx
@@ -13,12 +13,33 @@ function Square(props){
         return (
             <button
                 className='square'
-                onClick={() => {
-                    props.onClick();
-                }} >
+                onClick={() =>  props.onClick() }>
                 {props.value}
             </button>
         );
+}
+
+
+//helper function to determine winner
+function Winner(squares){
+    const lines = [
+        [0 , 1 , 2],
+        [3 , 4 , 5],
+        [6 , 7 , 8],
+        [0 , 3 , 6],
+        [1 , 4 , 7],
+        [2 , 5 , 8],
+        [0 , 4 , 8],
+        [2 , 4 , 6],
+    ];
+    for( let i = 0 ; i < lines.length ; i++){
+        const [a , b , c] = lines[i];
+        if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        {
+            return squares[a];
+        }
+    }
+        return null;
 }
 
 class Board extends React.Component {
@@ -27,13 +48,20 @@ class Board extends React.Component {
         super();
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext : true,
         };
     }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares : squares});
+        if(this.state.xIsNext){
+            squares[i] = 'X';
+        }else{ squares[i] = 'O'}
+
+        this.setState({
+                squares : squares,
+                xIsNext : !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -45,7 +73,15 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player : X';
+
+        const winner = Winner(this.state.squares);
+        let status;
+        if( winner ){
+            status = 'Winner : ' + winner;
+        }else{
+             status = 'Next player : ' + ( this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div>
                 <div className="status">
