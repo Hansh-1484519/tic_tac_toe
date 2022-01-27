07d4e5ba4e2819ce7c6c0e92af40cf06f5b4feb7
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-27 12:17:51 +0530

    basic functionality of tic_tac_toe game are added

--- react_script.jsx
+++ react_script.jsx
@@ -93,16 +93,16 @@ class Game extends React.Component {
         super(props);
         this.state = {
             history: [{
-                squares: Array(9).fill(null),
+                squares: Array(9).fill(null)
             }],
             stepNumber : 0,
             xIsNext: true,
         }
     }
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice( 0 , this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = current.squares.slice( 0 , this.state.stepNumber + 1 );
+        const squares = current.squares.slice();
 
         if (Winner(squares) || squares[i]) {
             return;
